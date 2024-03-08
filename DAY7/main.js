34cc40ced7ea530908d36772fc9eb91f391d1a2d
d88277036BQ <darkprince67890@gmail.com>
2024-03-08 17:15:13 +0500

    day 7 complete

---
+++ DAY7/main.js
@@ -0,0 +1,23 @@
+"use strict";
+/* 🚀 Day 7 Challenge: Start Coding! 🚀
+
+Question 19: Dinner Guests: Use one of your programs from Exercises 14 through 18 to print a message indicating the number of people you are inviting to dinner.
+Explain & TIP: Arrays have a length property that tells you how many items are in the list. Use this to inform your guests how big the party will be. */
+console.log("\nxxxxxxxxxxxxxxxxxxxxxxxxxxxxx Day7_Q1: xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\n");
+let guests = ['Lee John', 'Victor', 'John Marvick', 'Aliya'];
+console.log(`${guests.length} Dinner is on me .`);
+/*Question 20: Think of something you could store in an array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+Explain & TIP: Arrays are perfect for keeping a collection of similar items. Choose a theme you like and list several examples. */
+console.log("\nxxxxxxxxxxxxxxxxxxxxxxxxxxxxx Day7_Q2: xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\n");
+let like_countries = ["Korea", "Japan", "Itlay", "Paris", "Uman"];
+console.log("I'd like to enjoy photos of these contries:", like_countries);
+/*Question 21: Think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+Explain & TIP: Objects in TypeScript can store various related data about a single entity. Think of an object as a container for characteristics of something, like a book or a car.
+ */
+console.log("\nxxxxxxxxxxxxxxxxxxxxxxxxxxxxx Day7_Q3: xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\n");
+let laptop = {
+    name: " HP",
+    color: "Black with Pink",
+    madeyear: 2024
+};
+console.log(`Laptop Details: \n Name  : ${laptop.name} \n Color : ${laptop.color} \n Year  : ${laptop.madeyear} \n `);
