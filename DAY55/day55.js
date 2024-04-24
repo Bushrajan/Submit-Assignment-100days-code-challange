43e4ec29bbd0cab719e4ecaf06a63982d7841d5f
bushrajan <darkprince67890@gmail.com>
2024-04-25 02:00:43 +0500

    day 55 complete

---
+++ DAY55/day55.js
@@ -0,0 +1,27 @@
+let a = "Bushra Jan";
+    a = "Tuba Jan";
+    console.log(a);
+/****************************************/
+    var c = 123;
+        c = 456;
+    console.log(c);
+/****************************************/
+    const d = "BUSHRAJAN_123";
+    console.log(d);
+/****************************************/
+    alert("Hello Bushra.... ");
+/****************************************/
+    let test = prompt("Test", '');
+    console.log(test);
+
+    let day = prompt(' How was your day today? ');
+    alert(`My day was ${day} .`);
+/****************************************/
+// The square brackets in syntax [...]
+// The square brackets around default in the syntax above denote that the parameter is optional, not required.
+
+// result = prompt(title, [_default]);
+/****************************************/
+    
+let isBoss = confirm("Are you the boss?");
+alert( isBoss ); // true if OK is pressed
