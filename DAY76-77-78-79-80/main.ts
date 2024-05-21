8378a38e79836f9ed3fda743274b790cc69e7e14
bushrajan <darkprince67890@gmail.com>
2024-05-21 20:33:36 +0500

    done

---
+++ DAY76-77-78-79-80/main.ts
@@ -0,0 +1,63 @@
+
+console.log(`\n\n^^^^^^^^^^^^Complete days 76-80 ^^^^^^^^^^^^^^^\n\n`);
+console.log(`xxxxxxxxxxxxx Day 75 xxxxxxxxxxxxxx`);
+console.log(`\nWhat is Internet?\nAns: Interconnection of networks from all over the world.\n\n `);
+console.log(`_______________________________________________________________________________________________________`);
+
+// mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm Day 77 mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
+
+
+console.log(`\nxxxxxxxxxxxxx Day 76 xxxxxxxxxxxxxx`);
+/*Day-76 of 100-Days-Of-Code Challenge
+Day-76 Task:
+Read and understand the following Visual Guide about DNS and DHCP.
+ */
+console.log(`\nWhat is DNS?\nAns: Domain Name system server ( DNS Server). Browser can't open any website without knowing its IP address for it. IP adress assigned to every device on interner. You can either type the IP Address to open, or provide the domain name and press enter which will then be used by your browser to find the IP address by going through several hoops. And then you will see the result on yourn device.`);
+console.log(`\nWhat is DHCP?\nAns: Dynamit Host Congiguration Protocol (DHCP). It works as through DHCP Client & DHCP server. When ever the new people come to your house and connect their devices with same wifi network that has unique IP Address, you have seen the process before but you didn't know what is going behind on your device and somehow surprise that it is connected. I will tell you that the process when you enter the passcode on new devices you have seen the authetication process going in seconds and there you go with the unlimited connection. The process behind  is called (DHCP) work.`);
+console.log(`_____________________________________________________________________________`);
+
+
+// mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm Day 77 mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
+
+console.log(`\nxxxxxxxxxxxxx Day 77 xxxxxxxxxxxxxx`);
+/* Day-77 of 100-Days-Of-Code Challenge
+Day-77 Task:
+Read and understand the following Visual Guide about SSL, TLS, and SSH protocols. */
+console.log(`\nSSL vs TLS vs SSH Protocols\nAns: (SSL) means Secure Sockets Layer. (TLS) means Transport Layer Security. SSL rechanged its name TSL\n\nSecure Shell (SSH) or Secure Shell is a network communication protocol that enables two computers to communicate (e.g http or hypertext transfer protocol, which is the protocol used to transfer hypertext such as web pages) and share data.\n`);
+console.log(`_____________________________________________________________________________`);
+
+
+// mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm Day 78 mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
+
+console.log(`\nxxxxxxxxxxxxx Day 78 xxxxxxxxxxxxxx`);
+/*Day-78 of 100-Days-Of-Code Challenge
+Day-78 Task:
+Read and understand the following Visual Guide about the Web Vitals.*/
+console.log(`\nWhat are Web Vitals?\nAns: Web Vitals is a Google initiative to provide unified guidance for web page quality signals that are essential to delivering a great user experience on the web. Although Core Web Vitals metrics evolve over time, currently, there are three main pillars developers should focus on: loading, interactivity, and visual stability. Those related metrics consist of Largest Contentful Paint (LCP), First Input Delay (FID) and Cumulative Layout Shift (CLS).`);
+console.log(`_______________________________________________________________________________________________________`);
+
+
+// mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm  Day 79 mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
+
+console.log(`\nxxxxxxxxxxxxx Day 79 xxxxxxxxxxxxxx`);
+/*Day-79 of 100-Days-Of-Code Challenge
+Day-79 Task:
+Read the following Articles with interactive video about Basics of Authentication and HTTP Basic Authentication. */
+console.log(`\nBasics of  Authentication\nAns: Basic authentication involves proving a user is who they say they are through authentication methods such as a username and password, biometric information such as facial recognition or fingerprint scans, and phone or text confirmations (which are most often used as part of two-factor authentication methods). `);
+console.log(`\nHTTP Basic Authentication\nAns: HTTP Basic authentication (BA) implementation is the simplest technique for enforcing access controls to web resources because it does not require cookies, session identifiers, or login pages; rather, HTTP Basic authentication uses standard fields in the HTTP header. Authentication is a protocol used in HTTP communication to verify that a client is who they say they are before providing the client access to a certain resource on the web.`);
+console.log(`_______________________________________________________________________________________________________`);
+
+// mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm Day 80 mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
+
+console.log(`\nxxxxxxxxxxxxx Day 80 xxxxxxxxxxxxxx`);
+/*Day-80 of 100-Days-Of-Code Challenge
+Day-80 Task:
+Read the following Article about the difference between Full Stack Developer or Software Engineer.*/
+console.log(`\nFull Stack Developer or Software Engineer - Which Way to Go?\nAns: Full-Stack Developer \nFocuses on end-to-end development of applications (both front-end and back-end)	
+Mostly limited to just front-end and back-end development
+\n\nSoftware Engineer\n
+Specializes and practices in one domain and has a deep knowledge of it.
+There are a broad range of domain options to choose from. e.g front-end, back-end, mobile, desktop, embedded systems. `);
+console.log(`_______________________________________________________________________________________________________`);
+
+
---
+++ DAY81/main.ts
@@ -0,0 +1,73 @@
+// mmmmmmmmmmmmmmmmmmmmmmmmmmmm Day 81 mmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
+
+console.log(`\nxxxxxxxxxxxxx Day 81 xxxxxxxxxxxxxx\n`);
+/*Day-81 of 100-Days-Of-Code Challenge
+Day-81 Task:
+Today's task is to learn about: */
+
+console.log(`\nReact Foundation\nReact is a JavaScript library for building interactive user interfaces. 
+\nAbout React and Next.js\n  
+Next.js is a flexible React framework that gives you building blocks to create fast, full-stack web applications.\n
+But what exactly do we mean by this? Let's spend some time expanding on what React and Next.js are and how they can help you build web applications.
+Building blocks of a web application
+There are a few things you need to consider when building modern applications. Such as:
+
+User Interface - how users will consume and interact with your application.
+Routing - how users navigate between different parts of your application.
+Data Fetching - where your data lives and how to get it.
+Rendering - when and where you render static or dynamic content.
+Integrations - what third-party services you use (for CMS, auth, payments, etc.) and how you connect to them.
+Infrastructure - where you deploy, store, and run your application code (serverless, CDN, edge, etc.).
+Performance - how to optimize your application for end-users.
+Scalability - how your application adapts as your team, data, and traffic grow.
+Developer Experience - your team's experience building and maintaining your application.
+For each part of your application, you will need to decide whether you will build a solution yourself or use other tools, such as packages, libraries, and frameworks.
+
+\nWhat is Next.js?\n Next.js is a React framework that gives you building blocks to create web applications. By framework, we mean Next.js handles the tooling and configuration needed for React, and provides additional structure, features, and optimizations for your application\n \nRendering User Interfaces (UI)\n To understand how React works, we first need a basic understanding of how browsers interpret your code to create (or render) user interfaces (UI). When a user visits a web page, the server returns an HTML file to the browser`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log(`______________________________________________________________________________`);
+// mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
+
+
+// 😎 Such a day, ----- Welcome back again after a minutes-- - recently,
+// I'm more ambitious toward my goal, and ofcource I'm doing work with all my effort and the weather is too hot to survive.I am hoping that you are will be surviving in this weather.Be patients Allah will give us a sweet fruit at the end when the time is right.Hope is every things we got.  
+// I have completed My Day - task 81 👍 
+// My inspirational teachers: Sir Ali Jawwad Sir Ahmed Raza Shaikh
+// Zia Khan - Ameen Alam - Daniyal Nagori - Asharib Ali - Hamzah Syed - Okasha Aijaz - Muhammad Irfan - Muhammad Daniyal Qasmi - Ahmer Arman - Muhammad Hamza - Ifra Shamim - Fahad Khan - Sheikh Hafsa Nadeem - Javeria Hassan - Badar Ahmed Sheikh - Muhammad Fahim Raza - Hamza Ahmed Sheikh - Mubashir Ali - Muhammad Osama - Maria Fajruddin - Manaheel Jamil - Muhammad Ahsaan Abbasi - Isfhan Ahmed - Hafiz Ali Ahmed - Emad Bin Mohiuddin - Muhammad Yazib - Muhammad Bilal - HAFIZ NABEEL - Mohammad Hadi - Abdul Haseeb - Rabeea Zubair - Hafsa Saleem - Ariba Memon - Samina Memon - Techverse51 - Mannal Rana - Governor Sindh Initiative for GenAI, Web3, and Metaverse
+
+
+
