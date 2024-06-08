7db249be1ef7baf8a2fb2f1737ec9ff69357387e
bushrajan <darkprince67890@gmail.com>
2024-06-08 13:50:06 +0500

    done with 100

---
+++ DAY100/main.ts
@@ -0,0 +1,28 @@
+/*Day-100 of 100-Days-Of-Code Challenge
+Day-100 Task:
+Today's task is to learn about: */
+console.log(`\nxxxxxxxxxxxxxxxxxxxxxxxxx DAY100 xxxxxxxxxxxxxxxxxxxxxxxxx\n
+******************** Next's Step *********************** 
+-----------------------------------------
+Next Steps
+Congratulations! You've completed the Next.js dashboard course where you learned about the main features of Next.js and best practices for building web applications.But this is just the beginning—Next.js has many other features. It's designed to help you build small side projects, your next startup idea, or even large-scale applications with your team.
+Here are some resources to continue exploring Next.js:
+Next.js Documentation
+---------------------------
+Next.js Templates:
+*)_ Admin Dashboard Template
+*)_ Next.js Commerce
+*)_ Blog Starter Kit
+*)_ Nextra: Docs Starter Kit
+*)_ Image Gallery Starter
+*)_ Next.js Repository
+*)_ Vercel YouTube
+*)_ Next.js App Router: Routing, Data Fetching, Caching
+*)_ Performance in React and Next.js (Lydia Hallie)
+*)_ Vercel Product Walkthrough
+*)_ Share your Next.js app
+
+We encourage you to share the app you built in this tutorial on X. If you do, please mention our team at @nextjs so that we can take a look! We'd love to get your feedback on this course as well.
+We hope you enjoyed this course, and we encourage you to continue learning - by building.`);
+
+//----------------------------------------------------------
