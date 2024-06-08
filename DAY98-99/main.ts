9f78980ac65868a29ecf0a891a81687abeb5197e
bushrajan <darkprince67890@gmail.com>
2024-06-08 13:19:16 +0500

    day 98-99

---
+++ DAY98-99/main.ts
@@ -0,0 +1,46 @@
+/*Day-98 of 100-Days-Of-Code Challenge
+Day-98 Task:
+Today's task is to learn about: */
+console.log(`\nxxxxxxxxxxxxxxxxxxxxxxxxx DAY98 xxxxxxxxxxxxxxxxxxxxxxxxx\n
+  *********************** Authentication ************************
+What is authentication?
+-----------------------------------------
+Authentication is a key part of many web applications today. It's how a system checks if the user is who they say they are.
+A secure website often uses multiple ways to check a user's identity. For instance, after entering your username and password, the site may send a verification code to your device or use an external app like Google Authenticator. This 2-factor authentication (2FA) helps increase security. Even if someone learns your password, they can't access your account without your unique token.
+Authentication vs. Authorization, In web development, authentication and authorization serve different roles:
+*) Authentication is about making sure the user is who they say they are. You're proving your identity with something you have like a username and password.
+*) Authorization is the next step. Once a user's identity is confirmed, authorization decides what parts of the application they are allowed to use.
+*) So, authentication checks who you are, and authorization determines what you can do or access in the application.
+________________________________________________________________________________________________________________________________________________
+NextAuth.js
+--------------------------
+We will be using NextAuth.js to add authentication to your application. NextAuth.js abstracts away much of the complexity involved in managing sessions, sign-in and sign-out, and other aspects of authentication. While you can manually implement these features, the process can be time-consuming and error-prone. NextAuth.js simplifies the process, providing a unified solution for auth in Next.js applications.`);
+
+//----------------------------------------------------------
+/*Day-99 of 100-Days-Of-Code Challenge
+Day-99 Task:
+Today's task is to learn about: */
+console.log(`\nxxxxxxxxxxxxxxxxxxxxxxxxx DAY99 xxxxxxxxxxxxxxxxxxxxxxxxx\n
+  *****Adding MetaData*****
+  -----------------------------------------
+Metadata is crucial for SEO and shareability. In this chapter, we'll discuss how you can add metadata to your Next.js application.
+What is metadata
+------------------------
+In web development, metadata provides additional details about a webpage. Metadata is not visible to the users visiting the page. Instead, it works behind the scenes, embedded within the page's HTML, usually within the <head> element. This hidden information is crucial for search engines and other systems that need to understand your webpage's content better.
+_____________________________________________________________________________________
+Why is metadata important?
+----------------------------
+Metadata plays a significant role in enhancing a webpage's SEO, making it more accessible and understandable for search engines and social media platforms. Proper metadata helps search engines effectively index webpages, improving their ranking in search results. Additionally, metadata like Open Graph improves the appearance of shared links on social media, making the content more appealing and informative for users.
+
+Types of metadata
+---------------------
+There are various types of metadata, each serving a unique purpose. Some common types include:
+Title Metadata: Responsible for the title of a webpage that is displayed on the browser tab. It's crucial for SEO as it helps search engines understand what the webpage is about.
+
+code ------------------------------------------------------------------
+<title>Page Title</title>
+Description Metadata: This metadata provides a brief overview of the webpage content and is often displayed in search engine results.
+<meta name="description" content="A brief description of the page content." />
+Keyword Metadata: This metadata includes the keywords related to the webpage content, helping search engines index the page.`);
+
+//----------------------------------------------------------
